c37b58a95bf564deeb9e5d296285758f6efca4f7
Kyle Forsyth <kyleforsythcomedy@gmail.com>
2021-10-19 10:58:27 -0400

    First commit, slight restructure

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import Header from './Header.jsx';
+import Header from "./components/Header.jsx";
 
 const App = () => {
   return (
-    <div className='container'>
-      <Header/>
+    <div className="container">
+      <Header />
       <div className="container-view">Some activities should be here</div>
     </div>
   );
 };
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
 
 export default App;
