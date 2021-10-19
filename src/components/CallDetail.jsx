a2cfc258b30e8bdd2f26afb3140c61c72a6f27f5
Kyle Forsyth <kyleforsythcomedy@gmail.com>
2021-10-19 16:29:42 -0400

    style call detail page

--- src/components/CallDetail.jsx
+++ src/components/CallDetail.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import TimeAgo from "timeago-react";
+import { ImBackward, ImDrawer, ImPhone, ImUser } from "react-icons/im";
+import { HiPhoneOutgoing, HiPhoneIncoming, HiViewList } from "react-icons/hi";
 
 const CallDetail = (props) => {
   const { view, setView } = props;
@@ -10,7 +13,48 @@ const CallDetail = (props) => {
 
   return (
     <div className="callContainer">
-      <button onClick={returnHome}>Back</button>
+      <div className="callProfile">
+        <div className="userImage">
+          <ImUser size={75} />
+        </div>
+        <div className="userNumber">
+          {view.direction === "inbound" && (
+            <div className="callIcon">
+              <HiPhoneIncoming size={30} color={"crimson"} />
+              <h1>{view.from}</h1>
+            </div>
+          )}
+          {view.direction === "outbound" && (
+            <div className="callIcon">
+              <HiPhoneOutgoing size={30} color={"green"} />
+              <h1>{view.to}</h1>
+            </div>
+          )}
+        </div>
+        <div className="callData">
+          {view.via && <h2>{view.via}</h2>}
+          {view.duration && (
+            <div>
+              <p>{view.duration} minutes</p>
+              <p>{view && <TimeAgo datetime={view.created_at} />}</p>
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="buttonContainer">
+        <button onClick={returnHome}>
+          <ImBackward size={30} />
+          <p>BACK</p>
+        </button>
+        <button onClick={returnHome}>
+          <ImPhone size={30} />
+          <p>CALL BACK</p>
+        </button>
+        <button onClick={returnHome}>
+          <ImDrawer size={30} />
+          <p>ARCHIVE</p>
+        </button>
+      </div>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import "./style/app.css";
 import "./style/header.css";
 import "./style/activity.css";
 import "./style/footer.css";
+import "./style/call_detail.css";
 
 import App from "./App.jsx";
