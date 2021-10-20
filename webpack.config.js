8900e0d4375ce582fd587761f46f20242104700e
Kyle Forsyth <kyleforsythcomedy@gmail.com>
2021-10-20 11:22:42 -0400

    small tweaks

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./public/index.html",
+      template: "./docs/index.html",
       filename: "./index.html",
     }),
   ],
