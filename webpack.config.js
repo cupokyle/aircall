2ec5a5bd05936d44379397175a15e99926dd1df5
Kyle Forsyth <kyleforsythcomedy@gmail.com>
2021-10-20 11:04:01 -0400

    Reconfigure for deployment with gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./docs/index.html",
+      template: "./public/index.html",
       filename: "./index.html",
     }),
   ],
