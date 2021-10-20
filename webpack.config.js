6e55d3dffb659d234a343a8a0ddf6d98c3b1eeba
Kyle Forsyth <kyleforsythcomedy@gmail.com>
2021-10-20 10:53:49 -0400

    reconfigure public direct for github pages deployment

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./public/index.html",
+      template: "./docs/index.html",
       filename: "./index.html",
     }),
   ],
